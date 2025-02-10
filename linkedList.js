77b0826bd4f1aeffd6244e06322a17a143a6aeda
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-02-10 15:56:44 +0530

    implement size,tail,head method

--- linkedList.js
+++ linkedList.js
@@ -1,58 +1,90 @@
 // constructor classes
 
 class LinkedList {
-    constructor(head = null){
-        this.head = head;
-    };
+  constructor(head = null) {
+    this.head = head;
+  }
 
-    append(value) {
-        if(this.head===null) {
-            let newNode = new Node(value);
-            this.head = newNode;
-            return;
-        }
+  append(value) {
+    if (this.head === null) {
+      let newNode = new Node(value);
+      this.head = newNode;
+      return;
+    }
+    let tempVariable = this.head;
+    while (tempVariable !== null) {
+      if (tempVariable.nextNode === null) {
+        let newNode = new Node(value);
+        tempVariable.nextNode = newNode;
+        return;
+      }
+      tempVariable = tempVariable.nextNode;
+    }
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+  }
+
+  size() {
+    let size = 0;
+    if (this.head === null) {
+      return size;
+    }
+    let tempVariable = this.head;
+    while (tempVariable !== null) {
+
+      if (tempVariable.nextNode === null || tempVariable.nextNode !== null) {
+            size++;
+      }
+      tempVariable = tempVariable.nextNode;
+    }
+    return size;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    if(this.head === null) {
+        return null;
+    }
+    else {
         let tempVariable = this.head;
-            while(tempVariable !== null) {
-                if(tempVariable.nextNode === null) {
-                    let newNode = new Node(value);
-                        tempVariable.nextNode = newNode;
-                        return;
-                }
-                            tempVariable = tempVariable.nextNode;                                
-            }     
+        while(tempVariable !== null) {
+            if(tempVariable.nextNode === null) {
+                return tempVariable;
+            }
+            tempVariable = tempVariable.nextNode;
         }
+    }
+  }
 
 
-        prepend(value) {
-            const newNode = new Node(value);
-                if(this.head === null) {
-                    this.head = newNode;
-                }   
-
-                else {
-                    newNode.nextNode = this.head;
-                    this.head = newNode;
-                }
-        }
-    };
 
 
+}
 
 class Node {
-    constructor(value = null ,nextNode = null) {
-            this.value = value;
-            this.nextNode = nextNode;
-    };
-};
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
 
-
-//creating/initializing a linkedlist 
+//creating/initializing a linkedlist
 
 let newLinkedList = new LinkedList();
 console.log(newLinkedList);
 newLinkedList.append("first node");
 newLinkedList.append("second node");
 newLinkedList.prepend("prepended value");
-
 console.log(newLinkedList);
-
+console.log(newLinkedList.tail())
