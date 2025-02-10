1cc118b3538aad2ae70e390e747978677832612c
Abhilash Patil <89824888+abhilash15500@users.noreply.github.com>
2025-02-10 15:18:48 +0530

    implement append feature

--- linkedList.js
+++ linkedList.js
@@ -1,26 +1,44 @@
 // constructor classes
 
 class LinkedList {
-    constructor(head = null,tail = null){
+    constructor(head = null){
         this.head = head;
-        this.tail = tail;
-    }
+    };
 
     append(value) {
+        if(this.head===null) {
+            let newNode = new Node(value);
+            this.head = newNode;
+            return;
+        }
+        let tempVariable = this.head;
+            while(tempVariable !== null) {
+                if(tempVariable.nextNode === null) {
+                    let newNode = new Node(value);
+                        tempVariable.nextNode = newNode;
+                        return;
+                }
+                            tempVariable = tempVariable.nextNode;                                
+            }     
+        }
+    };
 
-    }
-};
+
 
 class Node {
-    constructor(value,nextNode = null) {
-       this.value = value;
-    this.nextNode = nextNode;
+    constructor(value = null ,nextNode = null) {
+            this.value = value;
+            this.nextNode = nextNode;
     };
 };
 
 
-//creating a linkedlist 
+//creating/initializing a linkedlist 
+
 let newLinkedList = new LinkedList();
 console.log(newLinkedList);
-
+newLinkedList.append("first node");
+newLinkedList.append("second node");
+newLinkedList.append("third node");
+console.log(newLinkedList);
 
